5d97771f5345a8c897f9a774342a4e39dde3d3bf
Миша Пиштой <114359876+mpis228@users.noreply.github.com>
2023-05-20 14:54:44 +0300

    text+

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ class FatherW(customtkinter.CTkToplevel):
         self.button_cod = customtkinter.CTkButton(self, text="закодувати", command=self.cod)
         self.button_decod = customtkinter.CTkButton(self, text="розкодувати", command=self.decod)
         self.button_cod.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
-        self.button_decod.place(relx=0.75 , rely=0.5, anchor=tkinter.CENTER)
+        self.button_decod.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
         self.answer = None
 
     def chek_leb_and_del(self):
@@ -20,7 +20,6 @@ class FatherW(customtkinter.CTkToplevel):
             pass
         else:
             self.answer.destroy()
-
 
     def cod(self):
         self.chek_leb_and_del()
@@ -33,6 +32,7 @@ class FatherW(customtkinter.CTkToplevel):
         text = self.InputGet.get()
         self.answer = customtkinter.CTkLabel(self, text=self.main.decod(text))
         self.answer.pack()
+
 
 class WindowAtbash(FatherW):
     def __init__(self, *args, **kwargs):
@@ -47,14 +47,12 @@ class WindowAtbash(FatherW):
         self.answer.pack()
 
 
-
-
-
 class WindowCezar(FatherW):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Шифр Атбаш")
         self.main = tools.Cezar()
+
 
 class WindowPolibia(FatherW):
     def __init__(self, *args, **kwargs):
@@ -62,7 +60,12 @@ class WindowPolibia(FatherW):
         self.title("Шифр Атбаш")
         self.main = tools.Polibia()
 
+
 class App(customtkinter.CTk):
+
+    """главаний класс я би ще назвал його класс меню тому что що в нем прописана логика роботи с другими класами
+    просанна работа 3 кнопок"""
+
     def __init__(self):
         super().__init__()
         self.geometry('600x520')
@@ -70,26 +73,27 @@ class App(customtkinter.CTk):
         self.label = customtkinter.CTkLabel(self, text="Вибери шифр", width=300, height=300)
         self.label.pack()
         self.button_1 = customtkinter.CTkButton(self, text="Атбаш", command=self.open_atbash)
-        self.button_2 = customtkinter.CTkButton(self, text="Цезар", command=self.cezar)
-        self.button_3 = customtkinter.CTkButton(self, text="Полібія", command=self.polibi)
+        self.button_2 = customtkinter.CTkButton(self, text="Цезар", command=self.open_cezar)
+        self.button_3 = customtkinter.CTkButton(self, text="Полібія", command=self.open_polibi)
         self.button_1.place(relx=0.2, rely=0.5, anchor=tkinter.CENTER)
         self.button_2.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
         self.button_3.place(relx=0.8, rely=0.5, anchor=tkinter.CENTER)
         self.top_wind = None
-
+    """окно роботи с атбаш"""
     def open_atbash(self):
         if self.top_wind is None or not self.top_wind.winfo_exists():
             self.top_wind = WindowAtbash(self)
         else:
             self.top_wind.focus()
-
-    def cezar(self):
+    """окно роботи с цезарем"""
+    def open_cezar(self):
         if self.top_wind is None or not self.top_wind.winfo_exists():
             self.top_wind = WindowCezar(self)
         else:
             self.top_wind.focus()
 
-    def polibi(self):
+    """окно роботи с полібієм"""
+    def open_polibi(self):
         if self.top_wind is None or not self.top_wind.winfo_exists():
             self.top_wind = WindowPolibia(self)
         else:
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import interface
-import tools
-from tkinter import *
 
-def main():
-    answer = input("напишите что хотите закодувути")
+""""""
 
 if __name__ == "__main__":
     app = interface.App()
